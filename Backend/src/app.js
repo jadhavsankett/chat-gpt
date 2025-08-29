33c281e4dddb4791382da1b08741a30a6bbca4ed
student sanket25 <jadhavsanket5656@gamil.com>
2025-08-29 15:46:03 -0400

    server not response?

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -19,7 +19,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname,'..','public')))
+app.use(express.static(path.join(__dirname,'../public')))
 
 
 /* api's */
