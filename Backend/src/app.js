33f8e0d7a961c71ff15ae5d3a5f856cb5f5400c3
student sanket25 <jadhavsanket5656@gamil.com>
2025-08-29 15:39:15 -0400

    missing the public value

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -27,7 +27,7 @@ app.use('/api/auth',authRutes)
 app.use('/api/chat',chatRutes)
 
 
-app.get('*', (req, res) => {
+app.get('*name', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
